1833e8c2c8628b6d73c93889e4f60020006064c1
Brave <brave_chen@mail.chungyo.net>
2020-08-17 12:27:19 +0800

    add class notes and practice

--- 050_Loop/testFor.php
+++ 050_Loop/testFor.php
@@ -3,8 +3,8 @@
 for ($i = 1; $i <= 3; $i++) {
 	for ($j = 1; $j <= 4; $j++) {
 		echo "$i , $j <br>";
-		if (($i + $j) % 4 == 0)
-		    break;
+		// if (($i + $j) % 4 == 0)
+		//     break;
 	}
 	echo "-----<br>";
 }
