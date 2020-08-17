a82d335ae1dc420d43b3de0895e3f887787994d2
Brave <brave_chen@mail.chungyo.net>
2020-08-17 17:25:45 +0800

    add class notes and practice

--- 070_String_Functions/test_strlen.php
+++ 070_String_Functions/test_strlen.php
@@ -2,5 +2,17 @@
   header("content-type: text/html; charset=utf-8");
   $s = '許功蓋';
   echo $s."<br>";
-  echo strlen($s);
+  echo strlen($s)."<br>";
+  // strlen, substr, str_replace
+  
+  $s = "0123456789";
+  echo str_replace("12", "34", $s)."<br>";
+  echo substr($s, 3, 4)."<br>";
+  $pos = strpos($s, "012");
+  if($pos === false) {
+    echo "Not found!";
+  }
+  else {
+    echo "pos: $pos";
+  }
 ?>
